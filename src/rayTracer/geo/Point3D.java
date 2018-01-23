cec9ece024b2481d4e8e10ddbc423081f9f63f20
Benjamin DeMann <benjamin.demann@gmail.com>
2018-01-23 09:11:15 -0700

    implemented custom point3d

--- src/rayTracer/geo/Point3D.java
+++ src/rayTracer/geo/Point3D.java
@@ -2,58 +2,68 @@ package rayTracer.geo;
 
 public class Point3D {
 
-	public Point3D(double d, double e, double f) {
-		// TODO Auto-generated constructor stub
+	private final double _x;
+	private final double _y;
+	private final double _z;
+	
+	public Point3D(double x, double y, double z) {
+		this._x = x;
+		this._y = y;
+		this._z = z;
 	}
 
 	public Point3D multiply(double i) {
-		// TODO Auto-generated method stub
-		return null;
+		return new Point3D(this._x * i, this._y * i, this._z * i);
 	}
 
 	public Point3D subtract(Point3D b) {
-		// TODO Auto-generated method stub
-		return null;
+		b = b.multiply(-1);
+		return this.add(b);
 	}
 
 	public Point3D normalize() {
-		// TODO Auto-generated method stub
-		return null;
+		double mag = this.magnitude();
+		return this.multiply(1/mag);
 	}
 
-	public int dotProduct(Point3D z) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double dotProduct(Point3D b) {
+		double x = this._x * b._x;
+		double y = this._y * b._y;
+		double z = this._z * b._z;
+		return x + y + z;
 	}
 
-	public Point3D add(Point3D zOnY) {
-		// TODO Auto-generated method stub
-		return null;
+	public Point3D add(Point3D b) {
+		double x = _x + b._x;
+		double y = _y + b._y;
+		double z = _z + b._z;
+		return new Point3D(x, y, z);
 	}
 
 	public double magnitude() {
-		// TODO Auto-generated method stub
-		return 0;
+		double a = Math.pow(this._x, 2);
+		double b = Math.pow(this._y, 2);
+		double c = Math.pow(this._z, 2);
+		return Math.sqrt(a + b + c);
 	}
 
-	public Point3D crossProduct(Point3D subtract) {
-		// TODO Auto-generated method stub
-		return null;
+	public Point3D crossProduct(Point3D b) {
+		double x = this._y * b._z - this._z * b._y;
+		double y = this._z * b._x - this._x * b._z;
+		double z = this._x * b._y - this._y * b._x;
+		return new Point3D(x, y, z);
 	}
 
 	public double getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this._x;
 	}
 
 	public double getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this._y;
 	}
 
 	public double getZ() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this._z;
 	}
 
 }
